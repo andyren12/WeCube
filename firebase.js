38406b61db31bdf610939b6de8d11b8417fa71c7
Andy Ren <andyren33@gmail.com>
2025-10-12 00:41:56 -0700

    set git ignore

--- firebase.js
+++ firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAXNot8lRkBopzkeWUYXgMSYiEeTCQ-C9M",
-  authDomain: "wecube-ef14f.firebaseapp.com",
-  projectId: "wecube-ef14f",
-  storageBucket: "wecube-ef14f.firebasestorage.app",
-  messagingSenderId: "670331214677",
-  appId: "1:670331214677:web:b3d38a8ca8e715bb122ddc",
-  measurementId: "G-ZT1DRCSFPC",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
